4977e37822ec52a4db1ea34a4845d036eafb4202
FANIN-ROBERT FA SIBIRI AHMED <fa.fanin-robert21@inphb.ci>
2025-08-06 15:25:27 +0000

    modification

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
   optimizeDeps: {
-    entries: ["src/main.tsx", "src/tempobook/**/*"],
+    entries: ["src/main.tsx"],
   },
   plugins: [
     react(),
